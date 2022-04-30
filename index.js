a67c3bc34661960c05ead4426e997efc11117dfe
Jordan Halfman <jordanhalfman@gmail.com>
2022-04-29 20:33:33 -0700

    commit tree 482d6dc1a2a5577fa8e2fac8786d2f3c29795ac8

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+    let driversArray = [];
+    driversArray.push(drivers[0]);
+    driversArray.push(drivers[1]);
+    console.log(driversArray);
+    return driversArray;
+}
+
+const returnLastTwoDrivers = function(drivers){
+    let driversArray = [];
+    driversArray.push(drivers[drivers.length-2]);
+    driversArray.push(drivers[drivers.length-1]);
+    console.log(driversArray);
+    return driversArray;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+        return multiplier*fare;
+    }
+}
+
+function fareDoubler(fare) {
+    const huh = createFareMultiplier(2);
+    return huh(fare);
+}
+
+function fareTripler(fare) {
+    const huh1 = createFareMultiplier(3);
+    return huh1(fare);
+}
+
+function selectDifferentDrivers(drivers, otherFunction) {
+    return otherFunction(drivers);
+}
